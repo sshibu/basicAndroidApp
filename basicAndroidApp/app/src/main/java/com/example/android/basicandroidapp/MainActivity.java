7009184328a5a3488f33b3c63ce6c206164099b9
Sherin Shibu <sherin@Sherins-MacBook-Pro.local>
2017-07-19 14:54:45 -0700

    integrate Mobile Center

--- basicAndroidApp/app/src/main/java/com/example/android/basicandroidapp/MainActivity.java
+++ basicAndroidApp/app/src/main/java/com/example/android/basicandroidapp/MainActivity.java
@@ -2,11 +2,16 @@ package com.example.android.basicandroidapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.microsoft.azure.mobile.MobileCenter;
+import com.microsoft.azure.mobile.analytics.Analytics;
+import com.microsoft.azure.mobile.crashes.Crashes;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        MobileCenter.start(getApplication(), "a48ababc-9701-425c-b3a0-133d8a73ad37",
+                Analytics.class, Crashes.class);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
